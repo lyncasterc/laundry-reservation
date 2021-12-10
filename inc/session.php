6a625bb68aa63fd11d573c1906f5fae34017d44e
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-12-10 00:23:12 -0500

    write signin function

--- inc/session.php
+++ inc/session.php
@@ -19,12 +19,28 @@
         header("Location: ../index.php");
     }
 
+    function signin(){
+        $input_username = $_POST['username'];
+        $input_password = $_POST['password'];
+        $user_id = get_user_id($input_username, $input_password);
+
+        if($user_id){
+            set_session_user($user_id);
+            header("Location: ../index.php");
+        } else {
+            echo "USER DOES NOT EXIST";
+            //todo: figure out how to flash an error
+        }
+    }
+
     //URL Router
     if(isset($_POST['submit'])){
         $url = $_POST['url'];
 
         if($url === 'register'){
             register();
+        } else if ($url === 'signin'){
+            signin();
         }
         
     }
