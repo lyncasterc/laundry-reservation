50a0340158a9e9ffd793bbeb26f5a602532297cc
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-12-18 21:33:58 -0500

    .

--- inc/database.php
+++ inc/database.php
@@ -4,7 +4,7 @@
     function db_connect(){
         $servername = 'localhost:3306';
         $username = 'root';
-        $password = 'Treehugger106';
+        $password = 'root';
         $db = new mysqli($servername, $username, $password);
         if($db->connect_error){
             die('Connection failed: ') . $db->connect_error;
